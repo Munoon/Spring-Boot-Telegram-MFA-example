7b4db20e0b3f38f271cb76f34579233d29cf3617
Munoon <Munoongg@gmail.com>
2020-05-10 11:45:45 +0300

    Initial commit

---
+++ src/main/java/com/habr/telegrambotmfa/TelegramBotMfaApplication.java
@@ -0,0 +1,11 @@
+package com.habr.telegrambotmfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotMfaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotMfaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/habr/telegrambotmfa/TelegramBotMfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habr.telegrambotmfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotMfaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
